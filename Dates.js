94146d0150d1afc8aa0808ea61396d47fa90acfe
SekhAzharHossain <137325639+SekhAzharHossain@users.noreply.github.com>
2023-09-21 15:34:07 +0000

    Dates

---
+++ Dates.js
@@ -0,0 +1,12 @@
+// // to get the date we use Date method followed by new keyword
+// let mydate=new Date()
+// console.log(mydate.toDateString())
+// console.log(mydate.toISOString())
+// console.log(mydate.toJSON())
+// console.log(mydate.toLocaleDateString())
+// console.log(mydate.toLocaleTimeString())
+// console.log(mydate.toString())
+
+// Time Stamp
+let myTimeStamp=Date.now()
+console.log(myTimeStamp)
